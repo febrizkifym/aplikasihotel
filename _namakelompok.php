0c8a380a0c8735e26dbef61a395e85d2e964a2ba
febrizkifym <febrizkifym@gmail.com>
2019-04-28 14:03:03 +0800

    templating

---
+++ _namakelompok.php
@@ -0,0 +1,7 @@
+<?php
+// Febrizki Mawikere (531417021)
+// Selfi T Abjul (531417009)
+// Raodah Masaudi (531417007)
+// Fahryna Cahyani Olabu (531417011)
+// Nurul putri Febryani Panigoro (531417029)
+?>
--- index.php
+++ index.php
@@ -1,7 +1,37 @@
-<?php
-// Febrizki Mawikere (531417021)
-// Selfi T Abjul (531417009)
-// Raodah Masaudi (531417007)
-// Fahryna Cahyani Olabu (531417011)
-// Nurul putri Febryani Panigoro (531417029)
-?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include('layout/head.php'); ?>
+</head>
+<body>
+
+<div class="wrapper">
+   
+    <?php include('layout/sidebar.php'); ?>
+
+    <div class="main-panel">
+        <?php include('layout/navbar.php'); ?>
+
+
+        <div class="content">
+            <div class="container-fluid">
+                <!--Content Goes Here-->
+            </div>
+        </div>
+
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                </p>
+            </div>
+        </footer>
+
+    </div>
+</div>
+
+
+</body>
+<?php include('layout/js.php'); ?>
+</html>
